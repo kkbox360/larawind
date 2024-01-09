4c5d611ea07b1215d0b0b8afe4dca9ff784559d5
kkbox360 <kkbox360kkbox360@gmail.com>
2024-01-09 23:51:01 +0000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [laravel(["public/css/app.css", "resources/js/app.js"])],
+});
